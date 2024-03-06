2a386d6207063de843a6400ffb9c3dee7ceaf6f1
Brahian Cruz Giraldo <c0881381@mylambton.ca>
2024-03-06 18:46:35 -0500

    Registration working, missing validations

---
+++ BL/ContextDB.cs
@@ -0,0 +1,66 @@
+﻿using MySql.Data.MySqlClient;
+using PatronusBazar.Models;
+
+namespace PatronusBazar.BL
+{
+    public class ContextDB
+    {
+
+        public bool CreateUser(User user)
+        {
+            bool response = true;
+            //testing connection
+            string connectionString = "Server=localhost;User ID=root;Password=1234;Database=patronusbazaar";
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "Insert INTO User(Name,Phone,Email,HogwartsHouse,Username,Password) VALUES (?,?,?,?,?,?)";
+
+                cmd.Parameters.AddWithValue("param1", user.Name);
+                cmd.Parameters.AddWithValue("param2", user.Phone);
+                cmd.Parameters.AddWithValue("param3", user.Email);
+                cmd.Parameters.AddWithValue("param4", user.HogwartsHouse);
+                cmd.Parameters.AddWithValue("param5", user.Username);
+                cmd.Parameters.AddWithValue("param6", user.Password);
+                int x = cmd.ExecuteNonQuery();
+                if (x == 0)
+                    response = false;
+            }
+            return response;
+        }
+
+        public int FindUser(string username, string password)
+        {
+            int response = 1;//User and password correct
+            //Testing connection
+            string connectionString = "Server=localhost;User ID=root;Password=1234;Database=patronusbazaar";
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("select password from User where Username='" + username + "'", conn);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        reader.Read();
+                        if (password != reader.GetString(0))
+                        {
+                            response = 0; // Password incorrect
+                        }
+
+                    }
+                    else
+                    {
+                        response = -1;//User incorrect
+                    }
+                }
+            }
+            return response;
+        }
+    }
+}
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
+using PatronusBazar.BL;
 using PatronusBazar.Models;
+using System.Data.Common;
 
 namespace PatronusBazar.Controllers
 {
@@ -9,73 +11,19 @@ namespace PatronusBazar.Controllers
     [Route("[controller]")]
     public class UserController : ControllerBase
     {
-        [HttpPost("User")]
+        readonly ContextDB db = new();
+
+        [HttpPost]
         public IActionResult Post([FromBody] User user)
         {
-            if (CreateUser(user))
-                return Ok(new { Message = "User register" });
+          
+            if (db.CreateUser(user))
+                return Ok(new { Message ="" });
 
+            return BadRequest(new { Error = "Error, try again or contact admin" });
 
-            return BadRequest(new { Message = "Error" });
         }
 
-        public bool CreateUser(User user)
-        {
-            bool response = true;
-            //testing connection
-            string connectionString = "Server=localhost;User ID=root;Password=1234;Database=patronusbazaar";
-
-            using (MySqlConnection conn = new MySqlConnection(connectionString))
-            {
-                conn.Open();
-                MySqlCommand cmd = new MySqlCommand();
-                cmd.Connection = conn;
-                cmd.CommandText = "Insert INTO User(Name,Phone,Email,HogwartsHouse,Username,Password) VALUES (ValName,ValPhone,ValEmail," +
-                    "ValHogwartsHouse,ValUsername,ValPassword)";
-
-                cmd.Parameters.Add("ValName", MySqlDbType.VarString).Value = user.Name;
-                cmd.Parameters.Add("ValPhone", MySqlDbType.VarString).Value = user.Phone;
-                cmd.Parameters.Add("ValEmail", MySqlDbType.VarString).Value = user.Email;
-                cmd.Parameters.Add("ValHogwartsHouse", MySqlDbType.VarString).Value = user.HogwartsHouse;
-                cmd.Parameters.Add("ValUsername", MySqlDbType.VarString).Value = user.Username;
-                cmd.Parameters.Add("ValPassword", MySqlDbType.VarString).Value = user.Password;
-                int x = cmd.ExecuteNonQuery();
-                if (x == 0)
-                    response = false;
-            }
-            return response;
-        }
-
-        public int FindUser(string username, string password)
-        {
-            int response = 1;//User and password correct
-            //Testing connection
-            string connectionString = "Server=localhost;User ID=root;Password=1234;Database=patronusbazaar";
-
-            using (MySqlConnection conn = new MySqlConnection(connectionString))
-            {
-                conn.Open();
-                MySqlCommand cmd = new MySqlCommand("select password from User where Username='" + username + "'", conn);
-
-                using (var reader = cmd.ExecuteReader())
-                {
-                    if (reader.HasRows)
-                    {
-                        reader.Read();
-                        if (password != reader.GetString(0))
-                        {
-                            response = 0; // Password incorrect
-                        }
-
-                    }
-                    else
-                    {
-                        response = -1;//User incorrect
-                    }
-                }
-            }
-            return response;
-        }
 
     }
 }
